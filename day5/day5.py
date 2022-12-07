a4ca22e462d73b725757b1dae17c2533d4e58fe7
Felix Fahrbach <Felix.Fahrbach@gmail.com>
2022-12-07 22:40:24 +0100

    Day 6 complete

--- day5/day5.py
+++ day5/day5.py
@@ -44,33 +44,40 @@ def moveCrates(movement):
             storage[stTo].append(unit)
         #print(f'Storage TO after {storage[stTo]}')
 
+    print('Part 1 \t', end='')
     for storageUnit in storage:
-        print(f'Part 1 {storageUnit[len(storageUnit) - 1]}')
-
+        print(f'{storageUnit[len(storageUnit) - 1]}', end='')
+    print('')
 
 def moveCrates2(movement):
+    storage = [                                     #Restore storage Values
+        ['F', 'H', 'B', 'V', 'R', 'Q', 'D', 'P'],   # 0
+        ['L', 'D', 'Z', 'Q', 'W', 'V'],             # 1
+        ['H', 'L', 'Z', 'Q', 'G', 'R', 'P', 'C'],   # 2
+        ['R', 'D', 'H', 'F', 'J', 'V', 'B'],        # 3
+        ['Z', 'W', 'L', 'C'],                       # 4
+        ['J', 'R', 'P', 'N', 'T', 'G', 'V', 'M'],   # 5
+        ['J', 'R', 'L', 'V', 'M', 'B', 'S'],        # 6
+        ['D', 'P', 'J'],                            # 7
+        ['D', 'C', 'N', 'W', 'V']                   # 8
+    ]
+
     for move in movement:
-        print(move)
         stFrom = move[1]-1                                  # starts counting at 1 --> -1 so its the same as index
         stTo = move[2]-1                                    # starts counting at 1 --> -1 so its the same as index
         indexAmount = len(storage[stFrom])-move[0]
-        print(f'stFrom {stFrom}')
-        print(f'Storage FROM before {storage[stFrom]}')
         crane = storage[stFrom][indexAmount:]               # load crane
         storage[stFrom] = storage[stFrom][:indexAmount]     # leave rest on storage
-        print(f'Crane Loaded {crane}')
-        print(f'Storage FROM after {storage[stFrom]}')
-        print(f'Storage TO before {storage[stTo]}')
         for unit in crane:
             storage[stTo].append(unit)
-        print(f'Storage TO after {storage[stTo]}')
 
+    print('Part 2 \t', end='')
     for storageUnit in storage:
-        print(f'Part 2 {storageUnit[len(storageUnit) - 1]}')
+        print(f'{storageUnit[len(storageUnit) - 1]}', end='')
 
 if __name__ == '__main__':
     # Part 1
-    # moveCrates(loadData())
+    moveCrates(loadData())
 
     # Part 2
     moveCrates2(loadData())
