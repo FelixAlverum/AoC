1858d56547eb18b416375c8eb061455d2f9b3b27
FahrbachFel <Felix.Fahrbach@gmail.com>
2022-12-08 14:58:45 +0100

    sync

--- day7/day7.py
+++ day7/day7.py
@@ -1,53 +1,62 @@
 # Structure of a node in the tree
     # Name     - Name of the Node
     # Type     - DIR; FILE
-    # Value    - DIRECTORY --> List; File --> Size Value
+    # Size     - File --> Size Value
+    # Value    - DIRECTORY --> List;
     # Parent   - Object of the Parent
 class Node:
     name = ''
-    type = ''
+    typ = ''
     value = ''
     parent = None
 
-    def __init__(self, name, type, value, parent):
+    def __init__(self, name, typ, value, parent):
         self.name = name
-        self.type = type
+        self.typ = typ
         self.value = value
         self.parent = parent
 
-def parseConsole(console, filesystem):
+def parseConsole(console, root, currentNode):
     currentPosConsole = 0
     while currentPosConsole < len(console):
         print(f'line {console[currentPosConsole]} consolePos {currentPosConsole}')
 
         if console[currentPosConsole] == '$ ls':
-            res = ls(console,currentPosConsole,filesystem, currentNode)
+            res = ls(console,currentPosConsole, currentNode)
             currentPosConsole = res[0]
-            filesystem=res[1]
+            currentNode=res[1]
         elif console[currentPosConsole] == '$ cd /':
             currentNode = root
             currentPosConsole = currentPosConsole + 1
         elif console[currentPosConsole] == '$ cd ..':
-            pass
-            #cd_out()
+            currentPosConsole = currentPosConsole + 1
+            if currentNode.parent == None:
+                continue
+            currentNode = currentNode.parent
         else:
-            #res = cd_in(console[currentPosConsole][5:], currentPosConsole, currentPosFilesys)
-            currentPosConsole = res[0]
-            currentPosFilesys = res[1]
+            currentNode = cd_in(console[currentPosConsole][5:], currentNode)
+            currentPosConsole = currentPosConsole + 1
     return None
 
-def ls(console,currentPosConsole,filesystem, currentNode):
+def ls(console,currentPosConsole, currentNode):
+    currentPosConsole = currentPosConsole + 1
     while currentPosConsole < len(console):
         line = console[currentPosConsole]
-        if line.startswith('$'):
-            return [currentPosConsole, filesystem]
+        print(line)
+        if console[currentPosConsole].startswith('$'):
+            return [currentPosConsole, currentNode]
         elif line.startswith('dir '):
             currentNode.value.append(Node(line[4:], 'DIR', [], currentNode))
             currentPosConsole = currentPosConsole + 1
         else:
             data = line.split(' ')
-            currentNode.value.append(data[1], 'FILE', int(data[0]), currentNode)
+            currentNode.value.append(Node(data[1], 'FILE', int(data[0]), currentNode))
             currentPosConsole = currentPosConsole + 1
+
+def cd_in(name, currentNode):
+    for dir in currentNode.value:
+        if dir.name == name:
+            return dir
 
 
 def loadData():
@@ -60,9 +69,7 @@ def loadData():
 if __name__ == '__main__':
     console = loadData()
 
-    root = Node('/', 'DIR', [], None)
-    currentNode = root
-
-    parseConsole(console, root)
+    filesystem = Node('/', 'DIR', [], None)
+    parseConsole(console, filesystem, filesystem)
 
     # Part 2
