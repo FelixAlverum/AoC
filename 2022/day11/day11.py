be5b732c723d336062214d0a340aa2c5dbac150e
FelixAlverum <Felix.Fahrbach@gmail.com>
2022-12-15 16:53:41 +0100

    sync

--- 2022/day11/day11.py
+++ 2022/day11/day11.py
@@ -1,3 +1,8 @@
+import math
+
+# inspect --> relive --> pass to monkey
+
+
 class monkey:
     items = []
     operation = 'no operation'
@@ -20,6 +25,7 @@ class monkey:
     def option(self):
         for stresslevel in self.items:
             item = stresslevel
+            #inspect item
             if self.operation == '+':
                 stresslevel += self.worryMultiplier
             elif self.operation == '*':
@@ -29,7 +35,8 @@ class monkey:
                     stresslevel *= self.worryMultiplier
 
             self.inspectedItemsCtr = self.inspectedItemsCtr + 1
-            if (stresslevel % self.divisior) == 0:
+            stresslevel = math.floor(stresslevel / 3)
+            if stresslevel % self.divisior == 0:
                 self.trueApe.items.append(item)
             else:
                 self.falseApe.items.append(item)
@@ -37,7 +44,7 @@ class monkey:
 
     def printMonkey(self, monkeyNr):
         if len(self.items) == 0:
-            print(f'Monkey {monkeyNr}. Nah its empty')
+            print(f'Monkey {monkeyNr}. Nah its empty, Inspected Items Ctr: {self.inspectedItemsCtr}')
         else:
             print(f'Monkey {monkeyNr}. items {self.items}, Inspected Items Ctr: {self.inspectedItemsCtr}')
 
@@ -56,39 +63,32 @@ if __name__ == '__main__':
     monkey1.setPassApes(monkey2, monkey5)
     monkey2.setPassApes(monkey4, monkey3)
     monkey3.setPassApes(monkey0, monkey7)
-    monkey4.setPassApes(monkey6, monkey3)
+    monkey4.setPassApes(monkey7, monkey3)
     monkey5.setPassApes(monkey4, monkey2)
     monkey6.setPassApes(monkey1, monkey5)
     monkey7.setPassApes(monkey0, monkey6)
 
     monkeys = [monkey0, monkey1, monkey2, monkey3, monkey4, monkey5, monkey6, monkey7]
 
-    ####### Testdata
+    #Testdata
 
-    monkey0 = monkey([74, 64, 74, 63, 53], '*', 7, 5)
-    monkey1 = monkey([69, 99, 95, 62], '*', 'old', 17)
-    monkey2 = monkey([59, 81], '+', 8, 7)
-    monkey3 = monkey([50, 67, 63, 57, 63, 83, 97], '+', 4, 13)
-    monkey4 = monkey([61, 94, 85, 52, 81, 90, 94, 70], '+', 3, 19)
-    monkey5 = monkey([69], '+', 5, 3)
-    monkey6 = monkey([54, 55, 58], '+', 7, 11)
-    monkey7 = monkey([79, 51, 83, 88, 93, 76], '*', 3, 2)
+    monkey0 = monkey([79, 98], '*', 19, 23)
+    monkey1 = monkey([54, 65, 75, 74], '+', 6, 19)
+    monkey2 = monkey([79, 60, 97], '*', 'old', 13)
+    monkey3 = monkey([74], '+', 3, 17)
 
-    monkey0.setPassApes(monkey1, monkey6)
-    monkey1.setPassApes(monkey2, monkey5)
-    monkey2.setPassApes(monkey4, monkey3)
-    monkey3.setPassApes(monkey0, monkey7)
-    monkey4.setPassApes(monkey6, monkey3)
-    monkey5.setPassApes(monkey4, monkey2)
-    monkey6.setPassApes(monkey1, monkey5)
-    monkey7.setPassApes(monkey0, monkey6)
+    monkey0.setPassApes(monkey2, monkey3)
+    monkey1.setPassApes(monkey2, monkey0)
+    monkey2.setPassApes(monkey1, monkey3)
+    monkey3.setPassApes(monkey0, monkey1)
 
-    monkeys = [monkey0, monkey1, monkey2, monkey3, monkey4, monkey5, monkey6, monkey7]
+    monkeys = [monkey0, monkey1, monkey2, monkey3]
 
     #########################
 
 
-    for i in range(0,20,1):
+
+    for i in range(1,20,1):
         print(f'Round {i+1}')
         for i, monkey in enumerate(monkeys):
             monkey.printMonkey(i)
@@ -96,8 +96,8 @@ if __name__ == '__main__':
             monkey.printMonkey(i)
         print()
 
-    sum = 0
+    monkeyBusiness = []
     for m in monkeys:
-        sum = sum + m.inspectedItemsCtr
-
-    print(f'Sum {sum}')
+        monkeyBusiness.append(m.inspectedItemsCtr)
+    monkeyBusiness.sort(reverse=True)
+    print(f'monkeybusiness {monkeyBusiness}, {monkeyBusiness[0]} * {monkeyBusiness[1]} = {monkeyBusiness[0] *monkeyBusiness[1]}')
