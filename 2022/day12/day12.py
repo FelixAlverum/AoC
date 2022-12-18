2203130f9f3e1abf8de75b307698b181730d9f19
Felix Fahrbach <Felix.Fahrbach@gmail.com>
2022-12-19 00:28:12 +0100

    day 12 p1 problem appended nodes in a loop which are declared in the loop are saved as none - python scopes will be my beginners death

--- 2022/day12/day12.py
+++ 2022/day12/day12.py
@@ -1,32 +1,31 @@
 import sys
 
 
-class distance:
-    endnode = None
-    distance = sys.maxsize
-
-    def __int__(self, endnode, distance):
-        self.endnode = endnode
-        self.distance = distance
-
-
 class node:
-    value = '.'
+    value = sys.maxsize
     x = -1
     y = -1
-    distances = []
     prevNode = None
 
-    def __init__(self, value, x, y):
-        self.value = value
-        self.x = x
-        self.y = y
+    # the distance to all adjacent nodes is 1 --> no distance variable needed
 
     def __init__(self, value, x, y, prevNode):
         self.value = value
         self.x = x
         self.y = y
         self.prevNode = prevNode
+
+    def __eq__(self, other):
+        if self.x == other.x & self.y == other.y:
+            return True
+        return False
+
+    def __str__(self):
+        if self.prevNode == None:
+            str = f'value {self.value} --> x = {self.x}; y = {self.y} --- parent = self'
+        else:
+            str = f'value {self.value} --> x = {self.x}; y = {self.y} --- parent {self.prevNode.value}'
+        return str
 
 
 def loadData():
@@ -48,43 +47,94 @@ def findUniquePos(heightmap, uniqueChar):
 
 
 def createNodeMap(heightmap, startnode, visitedNodes, unvisitedNodes):
-    #set all possible distances to other nodes
-    #1. Node above startnode
-    if startnode.y - 1 > - 1:
-        print('node above available')
-        sv = ord(heightmap[startnode.x][startnode.y])
-        na = ord(heightmap[startnode.x][startnode.y-1])
+    unvisitedNodes.append(startnode)
 
-        if sv + 1 == na | sv == na | sv-1 == na:
-            unvisitedNode = node(startnode.value+1, startnode.x, startnode.y-1, startnode)
-            startnode.distances.append(distance(unvisitedNode,1))
-    #2. Node right to startnode
-    if startnode.x + 1 < len(heightmap[startnode.x]):
-        print('node to the right available')
-    #3. Node below startnode
-    if startnode.y + 1 < len(heightmap):
-        print('node below available')
-    #4. Node left to startnode
-    if startnode.x - 1 > -1:
-        print('node to the left available')
+    while len(unvisitedNodes) > 0:
+        currentNode = unvisitedNodes[0]
 
-    #add node to already visited nodes
-    visitedNodes.append(startnode)
+        # 1. Node above currentNode
+        if currentNode.x - 1 > - 1:
+            unvisitedNodes = updateNode(heightmap, visitedNodes, unvisitedNodes, currentNode, 'up')
+
+        # 2. Node right to currentNode
+        if currentNode.y + 1 < len(heightmap[currentNode.x]):
+            unvisitedNodes = updateNode(heightmap, visitedNodes, unvisitedNodes, currentNode, 'right')
+
+        # 3. Node below currentNode
+        if currentNode.x + 1 < len(heightmap):
+            unvisitedNodes = updateNode(heightmap, visitedNodes, unvisitedNodes, currentNode, 'down')
+
+        # 4. Node left to currentNode
+        if currentNode.y - 1 > -1:
+            unvisitedNodes = updateNode(heightmap, visitedNodes, unvisitedNodes, currentNode, 'left')
+
+        unvisitedNodes = sorted(unvisitedNodes, key=lambda x: x.value)
+        for i, un in enumerate(unvisitedNodes):
+            if i == 0:
+                print()
+            print(f'{i+1} / {len(unvisitedNodes)} --- unvisited node: {un}')
+
+        # add node to already visited nodes
+        visitedNodes.append(currentNode)
+        # remove node from unvisited nodes
+        unvisitedNodes.remove(currentNode)
+
+    return visitedNodes
 
 
-def removeUnvisitedNodeEntry(unvisitedNodes,x,y):
-    for node in unvisitedNodes:
-        if node.x == x & node.y == y:
-            return unvisitedNodes.remove(node)
+def inNodelist(node, nodelist):
+    for lnode in nodelist:
+        if node.__eq__(lnode):
+            return True, lnode
+    return False, None
+
+
+def updateNode(heightmap, visitedNodes, unvisitedNodes, currentNode, direction):
+    if heightmap[currentNode.x][currentNode.y] == 'S':
+        cv = ord('a')
+    else:
+        cv = ord(heightmap[currentNode.x][currentNode.y])  # currentnode value
+    if direction == 'up':
+        directionNode = node(currentNode.value + 1, currentNode.x - 1, currentNode.y, currentNode)
+    elif direction == 'down':
+        directionNode = node(currentNode.value + 1, currentNode.x + 1, currentNode.y, currentNode)
+    elif direction == 'right':
+        directionNode = node(currentNode.value + 1, currentNode.x, currentNode.y + 1, currentNode)
+    else:  # --> left
+        directionNode = node(currentNode.value + 1, currentNode.x, currentNode.y - 1, currentNode)
+
+    # check if node was already visited
+    if inNodelist(directionNode, visitedNodes)[0]:
+        return unvisitedNodes
+
+    # check if node needs to be added or updated # TODO FEHLER BEIM löschen von Nodes
+    if heightmap[directionNode.x][directionNode.y] == 'S':
+        dv = ord('a')
+    else:
+        dv = ord(heightmap[directionNode.x][directionNode.y])  # direction node value
+    if (cv + 1 == dv) | (cv == dv) | (
+            cv - 1 == dv):  # check if currentNode is on the same level or +- 1 as directionNode
+        inList, lNode = inNodelist(directionNode, unvisitedNodes)
+        if inList:
+            if directionNode.value < lNode.value:  # update list node
+                print('update')
+                unvisitedNodes.remove(lNode)
+                unvisitedNodes.append(directionNode)
+        else:
+            print('new')
+            unvisitedNodes.append(directionNode)  # create new node
+
+    print(f'updateNode len(unvisitedNodes) {len(unvisitedNodes)}')
+    for i, un in enumerate(unvisitedNodes):
+        print(f'{i}. updateNode {un}')
+
+    return unvisitedNodes
+
 
 if __name__ == '__main__':
     heightmap = loadData()
 
     start = findUniquePos(heightmap, 'S')
     end = findUniquePos(heightmap, 'E')
-    print(f'start: {start}, end: {end}')
 
-    startnode = node(0, start[0], start[1])
-    visitedNodes = []
-    unvisitedNodes = []
-    nodemap = createNodeMap(heightmap, startnode, visitedNodes, unvisitedNodes)
+    nodemap = createNodeMap(heightmap, node(0, start[0], start[1], None), [], [])
